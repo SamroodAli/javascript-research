a08c0b84a1e8b544ed25b42fd3265745e0e679d2
samroodAli <devsamrood@gmail.com>
2023-03-18 01:15:40 +0530

    feat: add array find thisArg and move array this binding to dedicated file

--- this-in-array-methods.js
+++ this-in-array-methods.js
@@ -5,7 +5,7 @@ console.log("for Each example:");
   function () {
     // if this was an array function, there is no this binding even if pass the second argument
     console.log(this.name); // Samrood
-    // forEach, map takes in a second argument which is the 'this' binding, but not in arrow functions
+    // forEach, map,filter takes in a second argument which is the 'this' binding, but not in arrow functions
   },
   { name: "Samrood" }
 );
@@ -16,17 +16,36 @@ console.log("map example:");
   function () {
     // if this was an array function, there is no this binding even if pass the second argument
     console.log(this.name); // Samrood
-    // forEach, map takes in a second argument which is the 'this' binding, but not in arrow functions
+    // forEach, map ,filter takes in a second argument which is the 'this' binding, but not in arrow functions
   },
   { name: "Samrood" }
 );
 
+// other apis that takes'this' argument
+
+console.log("filter example");
+[1, 2, 3].filter(
+  function foo() {
+    console.log(this.name); // 'Samrood'
+  },
+  { name: "Samrood" }
+);
+
+console.log("find example");
+[1, 2, 3].find(
+  function foo() {
+    console.log(this.name); // 'Samrood'
+  },
+  { name: "Samrood" }
+);
+
+console.log("This is different in Array.reduce");
 console.log("reduce example");
 
 [1, 2, 3, 4].reduce(
   function (acc, curr) {
     // if this was an array function, there is no this binding even if pass the second argument
-    console.log(acc, this.name); // Samrood
+    console.log(this.name); // undefined
 
     return acc;
     // reduce doesn't have 'this' argument
